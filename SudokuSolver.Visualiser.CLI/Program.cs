011e98e0b4faf20282d0923404a7da215cceeec8
Joe Thompson <joseph.thompson>
2020-03-25 20:28:42 +0000

    fixed to be nicer on actual CLI

--- SudokuSolver.Visualiser.CLI/Program.cs
+++ SudokuSolver.Visualiser.CLI/Program.cs
@@ -11,19 +11,23 @@ namespace SudokuSolver.Visualiser.CLI
             {
                 Console.CursorVisible = false;
                 var sudoku = Generator.Generate(o.Difficulty, o.Seed);
+                var firstRun = true;
+
                 var solved = SolvingEngine.Solve(sudoku, () =>
                 {
                     // overwrite the last, 
                     // doing this way avoids flicker
-                    Console.SetCursorPosition(0,0);
+                    Console.SetCursorPosition(0, firstRun ? Console.CursorTop : Console.CursorTop - 11);
                     Console.Write(sudoku);
                     Thread.Sleep((int)o.Timeout);
+
+                    firstRun = false;
                 });
 
-                Console.Clear();
-                Console.WriteLine("###### SOLVED ######");
+                Console.SetCursorPosition(0, firstRun ? Console.CursorTop : Console.CursorTop - 11);
+                Console.WriteLine("###### SOLVED ###### ");
                 Console.Write(sudoku);
-                Console.WriteLine("###### SOLVED ######");
+                Console.WriteLine("###### SOLVED ###### ");
             });
     }
 }
